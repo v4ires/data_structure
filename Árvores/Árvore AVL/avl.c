898687652c5fb19913abaaf525e71cf7882a011f
viniciusaires7 <viniciusaires7@gmail.com>
2014-03-30 19:21:16 -0300

    Adcionado comentários

--- Árvores/Árvore AVL/avl.c
+++ Árvores/Árvore AVL/avl.c
@@ -1,8 +1,14 @@
-/**
-*Autor: Vinicius Aires Barros
-*Estrutura de Dados
-*Arvore Binaria de Busca Balanceada AVL
-*/
+/**********************************************************************************************
+** Os códigos aqui implementados são implementações das Estrutura de Dados implementados
+** durante as disciplinas de Estrutura de Dados I, Estrutura de Dados II e na Monitoria onde
+** implementei alguns desses códigos em conjunto com os alunos durante o ano de 2013 e inicio
+** de 2014. 
+**
+** Desenvolvido por:
+**                   Vinicius Aires Barros
+**
+** Contato: Vinícius Aires Barros (viniciusaires7@gmail.com)
+***********************************************************************************************/
 
 #include "avl.h"
 
